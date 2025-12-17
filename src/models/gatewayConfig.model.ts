a49e87c2a7abe85c9b80ebee9cebc6aa8264a2c5
Aryan <aryanshandilya10@gmail.com>
2025-12-17 16:58:30 +0530

    chore: added gateway config model

---
+++ src/models/gatewayConfig.model.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+import { PaymentGateway } from "../constants/payments.constants";
+
+export interface IGatewayConfig {
+  gateway: PaymentGateway;
+  base_url: string;
+  merchant_id?: string;
+  api_key?: string;
+  secret_key?: string;
+  use_proxy: boolean;
+  proxy_url?: string;
+  is_active: boolean;
+}
+
+export const gatewayConfigSchema = new Schema<IGatewayConfig>(
+  {
+    gateway: {
+      type: String,
+      enum: Object.values(PaymentGateway),
+      required: true,
+      unique: true,
+    },
+    base_url: {
+      type: String,
+      required: true,
+    },
+    merchant_id: {
+      type: String,
+    },
+    api_key: {
+      type: String,
+    },
+    secret_key: {
+      type: String,
+    },
+    use_proxy: {
+      type: Boolean,
+      default: false,
+    },
+    proxy_url: {
+      type: String,
+    },
+    is_active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const GatewayConfigModel = mongoose.model<IGatewayConfig>(
+  "GatewayConfig",
+  gatewayConfigSchema
+);

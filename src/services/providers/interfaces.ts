b1a1d12dd869bde50b5504aa1627e93dca980aee
Aryan <aryanshandilya10@gmail.com>
2025-12-17 17:07:59 +0530

    chore: added provider interface

---
+++ src/services/providers/interfaces.ts
@@ -0,0 +1,23 @@
+import type { PaymentGateway } from "../../constants/payments.constants";
+
+export interface CreatePaymentInput {
+  amount: number;
+  tradeId: string;
+  merchanTxnId: string;
+  currency: string;
+  callbackUrl?: string;
+}
+
+export interface PaymentCreationResult {
+  payment_url: string;
+  gateway_transaction_id?: string;
+  raw_gateway_response?: any;
+}
+
+export interface IGatewayProvider {
+  name: PaymentGateway;
+  createPayment(
+    input: CreatePaymentInput
+  ): Promise<PaymentCreationResult>;
+  verifySignature?(payload: any, signature: string): boolean;
+}

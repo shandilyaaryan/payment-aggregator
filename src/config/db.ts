0c2373d855b46e1ddb94514b2e74ad058852b178
Aryan <aryanshandilya10@gmail.com>
2025-12-19 16:12:42 +0530

    chore: update db connection logic

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI!;
-
 export const connectToDb = async () => {
+  const mongo_uri = process.env.MONGO_URI!;
+
+  if (!mongo_uri) {
+    throw new Error("Mongo db url not found in dotenv");
+  }
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(mongo_uri);
     console.log("Connected to Mongo DB successfully");
   } catch (error) {
     console.log("Error while connecting to Mongo DB", error);
